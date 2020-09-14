24bdc598aa52bb1f180a960af03d50524fa2f8d9
Luis Antonio <freitas@luisantonio.net>
2020-09-14 00:07:02 -0300

    adicionando bootstrap

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,16 @@
+// This is a manifest file that'll be compiled into application.css, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+// It is generally better to create a new file per JavaScript code.
+//
+
+//= require jquery
+//= require bootstrap-sprockets
+
+//= require_tree .
+//= require_self
