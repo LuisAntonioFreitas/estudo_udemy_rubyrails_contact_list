66c4237c87575222e6a3ccd84d0b838b9bdfe7d7
Luis Antonio <freitas@luisantonio.net>
2020-09-21 21:21:32 -0300

    ajustes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Impede que mensagens de erro atrapalhem o CSS nos formulários
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
